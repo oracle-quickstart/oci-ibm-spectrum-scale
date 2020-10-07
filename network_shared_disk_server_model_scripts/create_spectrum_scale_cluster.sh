3140ba9cf4bb81a68e2e0f000d5f4f695ad52ea4
Pinkesh Valdria <pvaldria@yahoo.com>
2020-10-06 19:25:39 -0700

    added support for multi-cluster spectrum scale to have seperate client only cluster

--- network_shared_disk_server_model_scripts/create_spectrum_scale_cluster.sh
+++ network_shared_disk_server_model_scripts/create_spectrum_scale_cluster.sh
@@ -26,7 +26,7 @@ if [ $? -eq 0 ] ; then
   done
   cat /root/node.stanza
 
-  mmcrcluster -N node.stanza -r /usr/bin/ssh -R /usr/bin/scp -C ss-demo01.privateb2.ibmssvcnv3.oraclevcn.com -A
+  mmcrcluster -N node.stanza -r /usr/bin/ssh -R /usr/bin/scp -C ss-storage-cluster -A
   sleep 30s
 
   diskArray=(b c d e f g h i j k l m n o p q r s t u v w x y z aa ab ac ad ae af ag)
@@ -126,16 +126,16 @@ if [ $? -eq 0 ] ; then
 
 
 
-mmstartup -N nsdNodes
-while [ `mmgetstate -a | grep "active" | wc -l` -ne $((nsdNodeCount)) ] ; do echo "waiting for server nodes of cluster to start ..." ; sleep 10s; done;
+  mmstartup -N nsdNodes
+  while [ `mmgetstate -a | grep "active" | wc -l` -ne $((nsdNodeCount)) ] ; do echo "waiting for server nodes of cluster to start ..." ; sleep 10s; done;
 
-mmumount fs1 -a
-sleep 15s
-mmmount fs1 -a
-sleep 15s
+  mmumount fs1 -a
+  sleep 15s
+  mmmount fs1 -a
+  sleep 15s
 
-mmstartup -N clientNodes
-while [ `mmgetstate -a | grep "active" | wc -l` -ne $((nsdNodeCount + clientNodeCount)) ] ; do echo "waiting for client nodes of cluster to start ..." ; sleep 10s; done;
+  mmstartup -N clientNodes
+  while [ `mmgetstate -a | grep "active" | wc -l` -ne $((nsdNodeCount + clientNodeCount)) ] ; do echo "waiting for client nodes of cluster to start ..." ; sleep 10s; done;
 
 
   # Consolidate, since both failure groups needs to be in the same file, for the command the work.
@@ -161,30 +161,45 @@ while [ `mmgetstate -a | grep "active" | wc -l` -ne $((nsdNodeCount + clientNode
   sleep 15s
   df -h
 
-### CES Nodes
-## mmaddnode needs to be ran on a node which is already part of the cluster
-# Step.  Assign the quorum role to one of protocol node and the manager role to both nodes.
-for node in `cat /tmp/cesnodehosts` ; do
-  mmaddnode -N $node
-  mmchlicense server --accept -N $node
-  mmchnode --manager -N $node
-  mmstartup -N $node
-done
-while [ `mmgetstate -a  | grep "$cesNodeHostnamePrefix" | grep "active" | wc -l` -lt $((cesNodeCount)) ] ; do echo "waiting for ces nodes of cluster to start ..." ; sleep 10s; done;
+  ### CES Nodes
+  ## mmaddnode needs to be ran on a node which is already part of the cluster
+  # Step.  Assign the quorum role to one of protocol node and the manager role to both nodes.
+  for node in `cat /tmp/cesnodehosts` ; do
+    mmaddnode -N $node
+    mmchlicense server --accept -N $node
+    mmchnode --manager -N $node
+    mmstartup -N $node
+  done
+  while [ `mmgetstate -a  | grep "$cesNodeHostnamePrefix" | grep "active" | wc -l` -lt $((cesNodeCount)) ] ; do echo "waiting for ces nodes of cluster to start ..." ; sleep 10s; done;
 
 
-# To ensure pmsensors is installed on all nodes.
-#   mmdsh -N all "rpm -qa | grep gpfs.gss.pmsensors"
-# To ensure pmcollector is installed on only GUI mgmt nodes.
-#   mmdsh -N all "rpm -qa | grep gpfs.gss.pmcollector"
+  # To ensure pmsensors is installed on all nodes.
+  #   mmdsh -N all "rpm -qa | grep gpfs.gss.pmsensors"
+  # To ensure pmcollector is installed on only GUI mgmt nodes.
+  #   mmdsh -N all "rpm -qa | grep gpfs.gss.pmcollector"
 
-for node in `cat /tmp/mgmtguinodehosts` ; do
-  mmaddnode -N $node
-  mmchlicense client --accept -N $node
-  # recommended - 32G,  if node has memory
-  #mmchconfig pagepool=32G -N $node
-  mmchconfig pagepool=10G -N $node
-done
+  for node in `cat /tmp/mgmtguinodehosts` ; do
+    mmaddnode -N $node
+    mmchlicense client --accept -N $node
+    # recommended - 32G,  if node has memory
+    #mmchconfig pagepool=32G -N $node
+    mmchconfig pagepool=10G -N $node
+  done
+
+  if [ $clientNodeCount -eq 0 ]; then
+    mmauth genkey new
+    mmauth update . -l AUTHONLY
+    cp /var/mmfs/ssl/id_rsa.pub /home/opc/owningCluster_id_rsa.pub
+    chown opc:opc /home/opc/owningCluster_id_rsa.pub
+
+    # run the below on ss-server-1 node, after the ss-compute-only cluster is provisioned and you have the value of accessingClusterName and accessingCluster_id_rsa.pub file copied to ss-server-1 server. example:
+    # filesystemName=fs1
+    # accessingClusterName=ss-compute-only.fs.gpfs.oraclevcn.com
+    # accessingClusterAuthPublicKeyFilePath=/home/opc/accessingCluster_id_rsa.pub
+    # mmauth add $accessingClusterName -k $accessingClusterAuthPublicKeyFilePath
+    # mmauth grant $accessingClusterName -f ${filesystemName}
+  fi
+
 
 fi
 
